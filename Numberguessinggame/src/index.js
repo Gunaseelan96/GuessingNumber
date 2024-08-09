b1d96ddba5617de063665be035243e606e694610
Gunaseelan96 <129363006+Gunaseelan96@users.noreply.github.com>
2024-08-09 15:24:37 +0530

    Update index.js

--- Numberguessinggame/src/index.js
+++ Numberguessinggame/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Form from './Form'
 import reportWebVitals from './reportWebVitals';
 import Gamepage from './GamePage';
 
