bfde395fd194cfa0c68963b6c8593245567db565
Gunaseelan96 <129363006+Gunaseelan96@users.noreply.github.com>
2024-08-09 15:25:15 +0530

    Update App.js

--- Numberguessinggame/src/App.js
+++ Numberguessinggame/src/App.js
@@ -5,12 +5,6 @@ function App()
  
    return(
     <>
-       <h1 id='data'>Hello i am from app component</h1> 
-       {/* <img src={img}></img>
-       <i class="fa-sharp fa-solid fa-heart-pulse fa-flip fa-2xl" style={{color: "#db0a0a",fontSize:"400px"}}></i> */}
-       {/* <video>
-         <source src={}></source>
-       </video> */}
     </>
    )
 }
